c613801cb4cd5851924b0bddd2d74dbcf204ae61
Ugur Emre Dogan <ue.dogan@gmail.com>
2023-11-29 20:21:15 -0800

    Front end added to fetch the tasks from the backend.

---
+++ frontend/todo/src/components/TaskList.jsx
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from 'react';
+
+const TaskList = () => {
+
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://127.0.0.1:8000/api/todos/');
+                const data = await response.json();
+                setTasks(data)
+            } catch (error) {
+                console.log('Error fetching todos:', error)
+            }
+        };
+
+        fetchData();
+    }, [tasks]);
+
+    return (
+        <>
+          <h1> Task List</h1>
+            {tasks.map( (task) => (
+                <p key={task.id}> {task.description} </p>
+            ))}
+        </>
+    );
+};
+
+export default TaskList;
