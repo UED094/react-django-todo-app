d060cd29c4ed783173f72824472978605a5a87b9
Ugur Emre Dogan <ue.dogan@gmail.com>
2023-12-11 20:43:29 -0800

    Tasklist component for react got updated.

--- frontend/todo/src/components/TaskList.jsx
+++ frontend/todo/src/components/TaskList.jsx
@@ -1,31 +1,30 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 
 const TaskList = () => {
+  const [tasks, setTasks] = useState([]);
 
-    const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:8000/api/todos/");
+        const data = await response.json();
+        setTasks(data);
+      } catch (error) {
+        console.log("Error fetching todos:", error);
+      }
+    };
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('http://127.0.0.1:8000/api/todos/');
-                const data = await response.json();
-                setTasks(data)
-            } catch (error) {
-                console.log('Error fetching todos:', error)
-            }
-        };
+    fetchData();
+  }, []);
 
-        fetchData();
-    }, [tasks]);
-
-    return (
-        <>
-          <h1> Task List</h1>
-            {tasks.map( (task) => (
-                <p key={task.id}> {task.description} </p>
-            ))}
-        </>
-    );
+  return (
+    <>
+      <h1> Task List</h1>
+      {tasks.map((task) => (
+        <p key={task.id}> {task.description} </p>
+      ))}
+    </>
+  );
 };
 
 export default TaskList;
